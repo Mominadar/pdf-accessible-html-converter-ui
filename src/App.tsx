9f4eb6fb4b643d87a74489223c8bf78221191c9f
Momina Babar <mominababar97@gmail.com>
2025-03-26 00:21:05 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import IndexPage from "@/pages/index";
 import "react-toastify/dist/ReactToastify.css";
+import "@/styles/globals.css";
 
 function App() {
   return (
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,53 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 
-// https://vitejs.dev/config/
+dotenvExpand.expand(dotenv.config())
+
+const jsToJsx = () => {
+  return {
+    name: "treat-js-files-as-jsx",
+    async transform(code:string, id:string) {
+      if (!id.match(/src\/.*\.js$/)) return null;
+
+      // Use the exposed transform from vite, instead of directly
+      // transforming with esbuild
+      return transformWithEsbuild(code, id, {
+        loader: "jsx",
+        jsx: "automatic",
+      });
+    },
+  };
+};
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    jsToJsx(),
+    tsconfigPaths(),
+    federation({
+      name: "remote_app",
+      filename: "remoteEntry.js",
+      // expose your module. This should be the main component of your project, 
+      // not the one having <React.StrictMode> but the one with your actual logic
+      // so exposing App.tsx not main.tsx
+      exposes: {
+         './PdfAccessibleHtml': './src/App' // left side name of module to expose can be your choice, right side is the path to the component
+      },
+      shared: ['react','react-dom']
+    })
+  ],
+  base: process.env.NODE_ENV == 'production' ? process.env.VITE_BASE_URL_PROD : process.env.VITE_BASE_URL,
+  build: {
+    rollupOptions: {
+      external:[],
+    },
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  }
 })
